17761214cea3834ab1dcea3614c2ac0efd0dc4a2
saposabio23 <ehtomer@gmail.com>
2022-10-10 01:07:09 +0200

    nigh time improved

--- assets/script.js
+++ assets/script.js
@@ -99,7 +99,19 @@ function isItNight() {
     var hr = day.getHours();
     console.log(hr);
     if ((hr == 0) || (hr == 1) || (hr == 2) || (hr == 3) || (hr == 4) || (hr == 5) || (hr == 6)){
-
+      sddfdf()
+      function sddfdf() {
+        document.title = 'Sleeping.';
+          setInterval( function(){
+            document.title = 'Sleeping..';
+              setInterval( function(){
+                document.title = 'Sleeping...';
+                setInterval( function(){
+                  document.title = 'Sleeping... 💤';
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }
     }
     if ((hr == 7) || (hr == 8) || (hr == 9) || (hr == 10) || (hr == 11) || (hr == 12) || (hr == 13) || (hr == 14) || (hr == 15) || (hr == 16) || (hr == 17) || (hr == 18) || (hr == 19) || (hr == 20) || (hr == 21) || (hr == 22) || (hr == 23)){
       $nighScreen.classList.add("night")
