27f35b7f2cafa1dacb00ffe30f406001842a232e
saposabio23 <ehtomer@gmail.com>
2022-10-09 23:20:30 +0200

    upload touch

--- assets/script.js
+++ assets/script.js
@@ -59,3 +59,19 @@ function copyMail() {
         }, 1000);
       }, 1500);
 }
+
+
+// /* -----------------------------------
+// mouse feedback
+// -------------------------------------- */
+
+document.addEventListener("mousemove", event => {
+  document.getElementsByClassName("cursor")[0].style.transform = "translate(" + event.pageX + "px, " + event.pageY + "px)";
+})
+
+document.addEventListener("click", event => {
+  document.getElementsByClassName("cursor")[0].classList.add("click")
+  setTimeout(() => {
+    document.getElementsByClassName("cursor")[0].classList.remove("click")
+  }, 330)
+})
