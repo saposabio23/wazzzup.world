3b1a3b7658ccd82f3995ee9893af307a475fcc81
saposabio23 <ehtomer@gmail.com>
2022-10-10 00:29:28 +0200

    updated night mode

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 /* -----------------------------------
 IPHONE HEIGHT
 // -------------------------------------- */
-// window.onload = mobileWindow();
+window.onload = isItNight();
 
 // function mobileWindow() {
 //   let vh = window.innerHeight * 0.01;
@@ -65,13 +65,45 @@ function copyMail() {
 // mouse feedback
 // -------------------------------------- */
 
-document.addEventListener("mousemove", event => {
-  document.getElementsByClassName("cursor")[0].style.transform = "translate(" + event.pageX + "px, " + event.pageY + "px)";
-})
+function touch() {
+  document.addEventListener("mousemove", event => {
+    document.getElementsByClassName("cursor")[0].style.transform = "translate(" + event.pageX + "px, " + event.pageY + "px)";
+  })
 
-document.addEventListener("click", event => {
-  document.getElementsByClassName("cursor")[0].classList.add("click")
-  setTimeout(() => {
-    document.getElementsByClassName("cursor")[0].classList.remove("click")
-  }, 330)
-})
+  document.addEventListener("click", event => {
+    document.getElementsByClassName("cursor")[0].classList.add("click")
+    setTimeout(() => {
+      document.getElementsByClassName("cursor")[0].classList.remove("click")
+    }, 330)
+  })
+}
+// /* -----------------------------------
+// TIMER
+// -------------------------------------- */
+    function timer() {var second = 0;
+      function pad ( value ) { return value > 9 ? value : "0" + value; }
+      setInterval( function(){
+          document.getElementById("seconds").innerHTML=pad(++second%60);
+          document.getElementById("minutes").innerHTML=pad(parseInt(second/60,10));
+      }, 1000);
+    }
+
+// /* -----------------------------------
+// SLEEP WEB
+// -------------------------------------- */
+
+
+function isItNight() {
+  var $nighScreen = document.getElementById("nightscreen");
+    var day = new Date();
+    var hr = day.getHours();
+    console.log(hr);
+    if ((hr == 0) || (hr == 1) || (hr == 2) || (hr == 3) || (hr == 4) || (hr == 5) || (hr == 6)){
+
+    }
+    if ((hr == 7) || (hr == 8) || (hr == 9) || (hr == 10) || (hr == 11) || (hr == 12) || (hr == 13) || (hr == 14) || (hr == 15) || (hr == 16) || (hr == 17) || (hr == 18) || (hr == 19) || (hr == 20) || (hr == 21) || (hr == 22) || (hr == 23)){
+      $nighScreen.classList.add("night")
+      timer()
+      touch()
+    }
+  }
