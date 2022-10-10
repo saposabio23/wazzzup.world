b9987204455b35cf1eed24625c240d5539a114a8
saposabio23 <ehtomer@gmail.com>
2022-10-11 01:13:48 +0200

    sleeping mode

--- assets/script.js
+++ assets/script.js
@@ -98,12 +98,22 @@ function isItNight() {
     var day = new Date();
     var hr = day.getHours();
     console.log(hr);
-    if ((hr == 0) || (hr == 1) || (hr == 2) || (hr == 3) || (hr == 4) || (hr == 5) || (hr == 6)){
+    if ((hr == 1) || (hr == 1) || (hr == 2) || (hr == 3) || (hr == 4) || (hr == 5) || (hr == 6)){
         document.title = 'Sleeping... 💤';
+        offAtNight()
     }
-    if ((hr == 7) || (hr == 8) || (hr == 9) || (hr == 10) || (hr == 11) || (hr == 12) || (hr == 13) || (hr == 14) || (hr == 15) || (hr == 16) || (hr == 17) || (hr == 18) || (hr == 19) || (hr == 20) || (hr == 21) || (hr == 22) || (hr == 23)){
+    if ((hr == 7) || (hr == 8) || (hr == 9) || (hr == 10) || (hr == 11) || (hr == 12) || (hr == 13) || (hr == 14) || (hr == 15) || (hr == 16) || (hr == 17) || (hr == 18) || (hr == 19) || (hr == 20) || (hr == 21) || (hr == 22) || (hr == 0)){
       $nighScreen.classList.add("night")
       timer()
       touch()
+      offAtDay()
     }
   }
+  function offAtNight() {
+    document.querySelector('.content').remove();
+    document.querySelector('.sleep').remove();
+   }
+
+   function offAtDay() {
+    document.querySelector('.cursor').remove();
+   }
